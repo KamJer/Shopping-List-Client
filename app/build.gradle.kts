4d221aa4334d9b59f991b3f8716ae53620b499c9
KamJer <k.jerzynski@wp.pl>
2025-07-05 17:58:46 +0200

    change in target sdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "pl.kamjer.shoppinglist"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "pl.kamjer.shoppinglist"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 20
         versionName = "2.0"
 
